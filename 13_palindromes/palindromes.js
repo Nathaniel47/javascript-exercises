c1326e584f7cdebda1d8532c136f1654d2baa1d4
Nathaniel Ankomah Aidoo <nathanielankomahaidoo@gmail.com>
2026-01-21 18:25:18 +0000

    clean the input string

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
 const palindromes = function (string) {
     const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+    .toLowerCase()
+    .split()
+    .filter(character => alphanumeric.includes(character))
+    .join();
+
+    
     
     
 
